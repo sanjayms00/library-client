dd6a02661003f40c7c45aefebf87dc75a5029397
sanjayms00 <sanjaymsdev00@gmail.com>
2024-06-17 16:40:55 +0530

    client build

--- src/app/constants/constant.ts
+++ src/app/constants/constant.ts
@@ -1,3 +1,4 @@
 export const constant = {
-  baseUrl: 'http://localhost:3000/api/',
+  // baseUrl: 'http://localhost:3000/api/', //local
+  baseUrl: 'http://library.sanjayms.online/api/', //production
 };
